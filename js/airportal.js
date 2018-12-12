3934d5f8585db50b16f291025feff2001b7ca0c4
Mobius_Beta <405801769@qq.com>
2018-12-12 19:36:48 +0800

    18w50b
    添加支付页面（未完成）

--- js/airportal.js
+++ js/airportal.js
@@ -1,5 +1,5 @@
 var appName="AirPortal";
-var version="18w50a";
+var version="18w50b";
 console.info(appName+" 由 毛若昕 和 杨尚臻 联合开发。");
 console.info("版本："+version);
 var txtVer=document.getElementById("version");
