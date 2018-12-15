fd9cdf93e81b7594fa77609db314d58649f10320
Shangzhen Yang <20525492+shangzhenyang@users.noreply.github.com>
2018-12-15 08:45:06 -0500

    18w50e1
    简化代码

--- js/airportal.js
+++ js/airportal.js
@@ -1,5 +1,5 @@
 var appName="AirPortal";
-var version="18w50e";
+var version="18w50e1";
 console.info(appName+" 由 毛若昕 和 杨尚臻 联合开发。");
 console.info("版本："+version);
 txtVer.innerHTML=version;
@@ -502,7 +502,7 @@ btnClose2.onclick=function(){
 	},250);
 }
 function btnPay0State(){
-	if(document.getElementsByClassName('payItemSelected').length==2){
+	if(document.getElementsByClassName("selected").length==2){
 		btnPay0.style.pointerEvents="auto";
 		btnPay0.style.opacity="1";
 	}else{
@@ -510,53 +510,26 @@ function btnPay0State(){
 		btnPay0.style.opacity="0.5";
 	}
 }
-payItem1M.onclick=function() {
-	if(payItem1M.className!="payItemSelected"){
-		payItem1M.className="payItemSelected";
-		payItem3M.className="payItem";
-		payItem1Y.className="payItem";
+function payItemClick(element,className){
+	if(element.classList.contains("selected")){
+		element.classList.remove("selected")
 	}else{
-		payItem1M.className="payItem";
+		var elements=document.getElementsByClassName(className)
+		for(var i=0;i<elements.length;i++){
+			if(elements[i]==element){
+				element.classList.add("selected")
+			}else{
+				elements[i].classList.remove("selected")
+			}
+		}
 	}
-	btnPay0State()
+	btnPay0State();
 }
-payItem3M.onclick=function() {
-	if(payItem3M.className!="payItemSelected"){
-		payItem3M.className="payItemSelected";
-		payItem1M.className="payItem";
-		payItem1Y.className="payItem";
-	}else{
-		payItem3M.className="payItem";
-	}
-	btnPay0State()
+payItem1M.onclick=payItem3M.onclick=payItem1Y.onclick=function() {
+	payItemClick(this,"plan");
 }
-payItem1Y.onclick=function() {
-	if(payItem1Y.className!="payItemSelected"){
-		payItem1Y.className="payItemSelected";
-		payItem3M.className="payItem";
-		payItem1M.className="payItem";
-	}else{
-		payItem1Y.className="payItem";
-	}
-	btnPay0State()
-}
-payItemAli.onclick=function() {
-	if(payItemAli.className!="payItemSelected"){
-		payItemAli.className="payItemSelected";
-		payItemWechat.className="payItem";
-	}else{
-		payItemAli.className="payItem";
-	}
-	btnPay0State()
-}
-payItemWechat.onclick=function() {
-	if(payItemWechat.className!="payItemSelected"){
-		payItemWechat.className="payItemSelected";
-		payItemAli.className="payItem";
-	}else{
-		payItemWechat.className="payItem";
-	}
-	btnPay0State()
+payItemAli.onclick=payItemWechat.onclick=function() {
+	payItemClick(this,"method");
 }
 file.onchange=function(input){
 	var files=[];
