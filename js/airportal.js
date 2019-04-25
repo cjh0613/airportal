a4f049e1115b8ef019b773c11f2ee8a54ffabbb6
Shangzhen Yang <20525492+shangzhenyang@users.noreply.github.com>
2019-04-25 18:35:46 -0400

    19w17a14

--- js/airportal.js
+++ js/airportal.js
@@ -1,6 +1,6 @@
 "use strict";
 var appName="AirPortal";
-var version="19w17a13";
+var version="19w17a14";
 var consoleGeneralStyle="font-family:Helvetica,sans-serif;";
 var consoleInfoStyle=consoleGeneralStyle+"color:rgb(65,145,245);";
 console.info("%c%s 由 毛若昕 和 杨尚臻 联合开发。",consoleInfoStyle,appName);
