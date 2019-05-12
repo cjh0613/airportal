8897b1ed4fb5184cc418c4a77582ba08e0682f84
Shangzhen Yang <20525492+shangzhenyang@users.noreply.github.com>
2019-05-12 14:08:11 -0400

    19w20a4

--- js/error.js
+++ js/error.js
@@ -1,8 +1,8 @@
 if(/(MicroMessenger|QQ)\//i.test(navigator.userAgent)){
 	location.hostname="ap.rthe.cn";
 }else{
-	var code=parseInt(location.pathname.substr(1));
-	if(code&&code>=1000){
+	var code=location.pathname.substr(1);
+	if(parseInt(code)&&code.length==4){
 		localStorage.setItem("code",code);
 	}
 	location.href="/";
